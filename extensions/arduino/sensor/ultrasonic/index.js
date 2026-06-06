cf5f8c92bcf33d13060ad6fcb2fd88d63e8a0bc6
devstembotix9892 <dev.stembotix@gmail.com>
2026-06-06 16:25:40 +0530

    rccar update

---
+++ extensions/arduino/sensor/ultrasonic/index.js
@@ -0,0 +1,26 @@
+const ultrasonic = formatMessage => ({
+    name: formatMessage({
+        id: 'ultrasonic.name',
+        default: 'Ultrasonic'
+    }),
+    extensionId: 'ultrasonic',
+    version: '1.0.0',
+    supportDevice: ['arduinoUno', 'arduinoNano', 'arduinoMini', 'arduinoLeonardo',
+        'arduinoMega2560', 'arduinoEsp32', 'arduinoEsp8266','ottoRobot','intermediateKit','iotAiKit','arduinoNano_arduinoUno'],
+    // author: 'STEMbotix',
+    iconURL: `asset/ultrasonic.png`,
+    description: formatMessage({
+        id: 'ultrasonic.description',
+        default: 'Standard ultrasonic distance measurement module.'
+    }),
+    featured: true,
+    blocks: 'blocks.js',
+    generator: 'generator.js',
+    toolbox: 'toolbox.js',
+    msg: 'msg.js',
+    library: 'lib',
+    tags: ['sensor'],
+    helpLink: 'https://openblockcc.gitee.io/wiki/main'
+});
+
+module.exports = ultrasonic;
